8f6a413d8e2cd90a933d561f8878b9fe2527daeb
h.khani <khanyhma6@gmail.com>
2025-08-12 14:05:33 +0330

    add users && connections

--- mypwa/src/app/components/pages/connection-status/connection-status.component.ts
+++ mypwa/src/app/components/pages/connection-status/connection-status.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-connection-status',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './connection-status.component.html',
   styleUrl: './connection-status.component.scss'
 })
 export class ConnectionStatusComponent {
+  connectionStatus: string = '';
+  isOnline: boolean = false;
 
+  checkConnection() {
+    this.isOnline = window.navigator.onLine;
+    this.connectionStatus = this.isOnline
+      ? '✅ شما آنلاین هستید'
+      : '❌ شما آفلاین هستید';
+  }
 }
--- mypwa/src/app/components/users/users.component.ts
+++ mypwa/src/app/components/users/users.component.ts
@@ -1,12 +1,38 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule, MatSuffix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, MatButtonModule, MatSuffix, MatInput, FormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+  users: string[] = [];
+  newUser: string = '';
 
+  ngOnInit() {
+    const savedUsers = localStorage.getItem('users');
+    if (savedUsers) {
+      this.users = JSON.parse(savedUsers);
+    }
+  }
+
+  addUser() {
+    if (this.newUser.trim()) {
+      this.users.push(this.newUser.trim());
+      localStorage.setItem('users', JSON.stringify(this.users));
+      this.newUser = '';
+    }
+  }
+
+  removeUser(index: number) {
+    this.users.splice(index, 1);
+    localStorage.setItem('users', JSON.stringify(this.users));
+  }
 }
