88c30afa3455ab8d169431418494ad172a5fa585
h.khani <khanyhma6@gmail.com>
2025-08-21 12:37:10 +0330

    icons in home

--- mypwa/src/app/components/home/home.component.ts
+++ mypwa/src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, RouterLink, CommonModule],
+  imports: [RouterOutlet, MatButtonModule, RouterLink, CommonModule, MatIcon],
 
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
